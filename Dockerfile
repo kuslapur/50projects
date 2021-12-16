aa276cc00d19c59c9b9770ad25c0f404a68154f2
Ubuntu <ubuntu@ip-172-31-28-21.us-east-2.compute.internal>
2021-12-16 17:23:50 +0000

    adding files

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM ubuntu
-RUN apt-get update
-RUN apt-get install -y tzdata
-RUN apt-get -y install apache2
-ADD . /var/www/html
-EXPOSE 80
-ENTRYPOINT apachectl -D FOREGROUND
-ENV name Devops test
--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-const nums = document.querySelectorAll('.nums span')
-const counter = document.querySelector('.counter')
-const finalMessage = document.querySelector('.final')
-const replay = document.querySelector('#replay')
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
 
-runAnimation()
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
+    if(e.target.classList.contains('rating')) {
+        removeActive()
+        e.target.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
 
-function resetDOM() {
-  counter.classList.remove('hide')
-  finalMessage.classList.remove('show')
-
-  nums.forEach((num) => {
-    num.classList.value = ''
-  })
-
-  nums[0].classList.add('in')
-}
-
-function runAnimation() {
-  nums.forEach((num, idx) => {
-    const nextToLast = nums.length - 1
-
-    num.addEventListener('animationend', (e) => {
-      if (e.animationName === 'goIn' && idx !== nextToLast) {
-        num.classList.remove('in')
-        num.classList.add('out')
-      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-        num.nextElementSibling.classList.add('in')
-      } else {
-        counter.classList.add('hide')
-        finalMessage.classList.add('show')
-      }
-    })
-  })
-}
-
-replay.addEventListener('click', () => {
-  resetDOM()
-  runAnimation()
 })
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support</p>
+    `
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}
