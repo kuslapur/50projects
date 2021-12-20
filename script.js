18cf28c79ba097e75fd93c0728faa2c57a3ddd30
Ubuntu <ubuntu@ip-172-31-28-21.us-east-2.compute.internal>
2021-12-20 17:27:35 +0000

    adding files

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
-const nums = document.querySelectorAll('.nums span')
-const counter = document.querySelector('.counter')
-const finalMessage = document.querySelector('.final')
-const replay = document.querySelector('#replay')
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'We Love Programming!'
+let idx = 1
+let speed = 300 / speedEl.value
 
-runAnimation()
+writeText()
 
-function resetDOM() {
-  counter.classList.remove('hide')
-  finalMessage.classList.remove('show')
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
 
-  nums.forEach((num) => {
-    num.classList.value = ''
-  })
+    idx++
 
-  nums[0].classList.add('in')
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
 }
 
-function runAnimation() {
-  nums.forEach((num, idx) => {
-    const nextToLast = nums.length - 1
 
-    num.addEventListener('animationend', (e) => {
-      if (e.animationName === 'goIn' && idx !== nextToLast) {
-        num.classList.remove('in')
-        num.classList.add('out')
-      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-        num.nextElementSibling.classList.add('in')
-      } else {
-        counter.classList.add('hide')
-        finalMessage.classList.add('show')
-      }
-    })
-  })
-}
-
-replay.addEventListener('click', () => {
-  resetDOM()
-  runAnimation()
-})
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
